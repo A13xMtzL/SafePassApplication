1575662b77163f797c9752b53bae3a9a739f83eb
Alejandro Martínez Luna <A01276785@tec.mx>
2023-10-09 10:54:48 +0200

    Added Eslint configuration

--- safe_pass_front/.eslintrc.cjs
+++ safe_pass_front/.eslintrc.cjs
@@ -1,17 +1,53 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  env: { browser: true, es2020: true, node: true },
+  plugins: ['react-refresh', 'react', 'react-hooks', 'notice', 'prettier'],
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  settings: {
+    react: { version: 'detect' },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
   rules: {
+    'no-unused-vars': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'notice/notice': [
+      'error',
+      {
+        mustMatch: '\\(c\\) URJC - Safe Pass [0-9]{0,4}, rights reserved.',
+        template: '// (c) URJC - Safe Pass <%= YEAR %>, rights reserved.\n\n',
+        messages: {
+          whenFailedToMatch:
+            "Couldn't find 'Copyright notice', are you sure you added it?",
+        },
+      },
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': [
+      'warn',
+      {
+        additionalHooks: '(useRecoilCallback|useRecoilTransaction_UNSTABLE)',
+      },
+    ],
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
