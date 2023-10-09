6320b86b6f05077db87e6176027caf8d8bc93378
HugoBond <72299419+HugoBond@users.noreply.github.com>
2023-10-09 15:52:18 +0200

    Password generator (#4)

---
+++ safe_pass_back/src/generator/generator.py
@@ -0,0 +1,34 @@
+import secrets
+import string
+
+def check_char(valid_chars,password):
+    """
+    Given a list of chars groups, checks that the password at least
+    have one char of each group. Returns a boolean.
+    """
+    valid =[]
+    for group in valid_chars:
+        group_valid = []
+        for c in group:
+            if c in password:
+                group_valid.append(True)
+            else:
+                group_valid.append(False)
+            if any(group_valid):
+                valid.append(True)
+            else:
+                valid.append(False)
+    return all(valid)
+def generator(selection,length):
+    """"
+    Selection string must be sanitized
+    Selection is a list containing the type of chars the password must have, ("lower","upper","number","special")
+    """
+    mode = {"lower": string.ascii_lowercase, "upper": string.ascii_uppercase, "number": string.digits, "special": string.punctuation}
+    valid_chars = ""
+    for arr in selection:
+        valid_chars += mode[arr]
+    password = ''.join(secrets.choice(valid_chars) for _ in range(length))
+    while check_char(selection, password):
+        password = ''.join(secrets.choice(valid_chars) for _ in range(length))
+    return password
