0eba4dc3be99f4a5f7481e5dab028b724402f9ba
Alejandro Martínez Luna <A01276785@tec.mx>
2023-10-14 01:02:53 +0200

    Fixed position of BackgroundCircles

--- safe_pass_front/src/componets/BackgroundCircles.jsx
+++ safe_pass_front/src/componets/BackgroundCircles.jsx
@@ -15,7 +15,7 @@ export default function BackgroundCircles(props) {
       transition={{
         duration: 2.5,
       }}
-      className="relative flex items-center justify-center"
+      className="absolute top-0 left-0 w-full h-full z-[-1] items-center justify-center flex"
     >
       <div className="absolute mt-52 h-[300px] w-[300px] rounded-full border border-[#47bcd128]" />
       <div className="absolute mt-52 h-[500px] w-[500px] rounded-full border border-[#47bcd123]" />
