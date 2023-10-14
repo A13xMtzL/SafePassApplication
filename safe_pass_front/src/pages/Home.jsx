12985d01f89f396a4e4a79b39045e492946121e1
iN3ast <72418870+iN3ast@users.noreply.github.com>
2023-10-14 15:06:40 +0200

    Front generator (#17)

--- safe_pass_front/src/pages/Home.jsx
+++ safe_pass_front/src/pages/Home.jsx
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <div>
       <h1 className="font-mono text-5xl text-slate-200">Safe Pass</h1>
+      
       <div className="max-w-20rem overflow-hidden rounded-md bg-slate-600 shadow-md">
-        <div className="bg-primary px-4 py-2 font-bold text-white">Header</div>
+     
         <div className="px-4 py-2">
-          <h4 className="mb-2 text-lg font-bold">Primary card title</h4>
+          <h4 className="mb-2 text-lg font-bold">Software de contraseñas seguras</h4>
           <p className="text-gray-700">
-            Some quick example text to build on the card title and make up the
-            bulk of the card&apos;s content.
+              Generación y <text>filtrado de  contraseñas seguras</text>filtrado de contraseñas seguras crea contraseñas robustas personalizadas.
           </p>
         </div>
       </div>
