9bfe2b55d3631f115a4fc9214f1e0c01443778ee
a.lopezg.2021 <a.lopezg.2021@alumnos.urjc.es>
2023-10-14 13:03:54 +0200

    Intento de cambio del Home

--- safe_pass_front/src/pages/Home.jsx
+++ safe_pass_front/src/pages/Home.jsx
@@ -3,14 +3,14 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="font-mono text-5xl text-slate-200">Safe Pass</h1>
-      <div className="max-w-20rem overflow-hidden rounded-md bg-slate-600 shadow-md">
-        <div className="bg-primary px-4 py-2 font-bold text-white">Header</div>
+      <h1 className="font-mono text-5xl text-slate-200 pb-2">Safe Pass</h1>
+      <div className="max-w-20rem overflow-hidden rounded-md bg-sky-950 shadow-md">
         <div className="px-4 py-2">
-          <h4 className="mb-2 text-lg font-bold">Primary card title</h4>
-          <p className="text-gray-700">
-            Some quick example text to build on the card title and make up the
-            bulk of the card&apos;s content.
+          <h4 className="mb-2 text-lg font-bold text-slate-200">El primer paso hacia una mayor seguridad</h4>
+          <p className="text-slate-200">
+            SafePass es un gestor de contraseñas que no solo te permite guardar tus credenciales, sino que también
+            puedes comprobar si tu contraseña ha sido filtrada, validar si una contraseña es segura
+            y generar contraseñas seguras de una manera fácil y rápida.
           </p>
         </div>
       </div>
