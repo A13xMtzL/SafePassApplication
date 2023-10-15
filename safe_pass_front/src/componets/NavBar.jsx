425e7b5e49eb93bc31e5daeb5d6206f426550faa
Alejandro Martínez Luna <A01276785@tec.mx>
2023-10-16 01:41:37 +0200

    Added path in NavBar for checking leaked passwords

--- safe_pass_front/src/componets/NavBar.jsx
+++ safe_pass_front/src/componets/NavBar.jsx
@@ -62,18 +62,27 @@ export default function NavBar() {
             </li>
             <li>
               <Link
+                to="/generator"
+                className="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+              >
+                Generador
+              </Link>
+            </li>
+            <li>
+              <Link
                 to="/validator"
                 className="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
               >
                 Validador
               </Link>
             </li>
+
             <li>
               <Link
-                to="/generator"
+                to="/leaked"
                 className="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
               >
-                Generador
+                Comprobar contraseñas vulneradas
               </Link>
             </li>
           </ul>
