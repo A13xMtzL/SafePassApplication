7184e78452967bf27b3840f002ad877cdc05e6a1
Alejandro Martínez Luna <A01276785@tec.mx>
2023-10-15 18:28:23 +0200

    Links on images (#22)

--- safe_pass_front/src/pages/Validator.jsx
+++ safe_pass_front/src/pages/Validator.jsx
@@ -1,7 +1,7 @@
 // (c) URJC - Safe Pass 2023, rights reserved.
 
 import React from 'react'
-import PasswordChecker from './PasswordChecker'
+import PasswordChecker from '../componets/PasswordChecker'
 
 export default function SafePass() {
   return (
