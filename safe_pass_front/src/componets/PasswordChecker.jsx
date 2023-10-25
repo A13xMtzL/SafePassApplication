eda9023e6d84d411e4a32b3f3927257b39e32614
Alejandro Martínez Luna <A01276785@tec.mx>
2023-10-25 16:09:18 +0200

    Changes on the logic of the component and added css glow

--- safe_pass_front/src/componets/PasswordChecker.jsx
+++ safe_pass_front/src/componets/PasswordChecker.jsx
@@ -1,80 +1,90 @@
-// (c) URJC - Safe Pass 2023, rights reserved.
-
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react';
+import classNames from 'classnames';
 
 export default function PasswordChecker() {
-  const [password, setPassword] = useState('')
-  const [passwordStrength, setPasswordStrength] = useState('')
-  const [showPassword, setShowPassword] = useState(false)
+  const [password, setPassword] = useState('');
+  const [passwordStrength, setPasswordStrength] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const checkPasswordStrength = useCallback(() => {
+    const hasUppercase = /[A-Z]/.test(password);
+    const hasLowercase = /[a-z]/.test(password);
+    const hasDigit = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*()_+{}[\]:;<>,.?~\\-]/.test(password);
+    const length = password.length;
+
+    if (
+      hasUppercase &&
+      hasLowercase &&
+      hasDigit &&
+      hasSpecialChar &&
+      length >= 14
+    ) {
+      setPasswordStrength('Fuerte');
+    } else if (
+      hasUppercase &&
+      hasLowercase &&
+      (hasDigit || hasSpecialChar) &&
+      length >= 10 &&
+      length < 14
+    ) {
+      setPasswordStrength('Media');
+    } else {
+      setPasswordStrength('Débil');
+    }
+  }, [password]);
 
   useEffect(() => {
-    const checkPasswordStrength = () => {
-      const hasUppercase = /[A-Z]/.test(password)
-      const hasLowercase = /[a-z]/.test(password)
-      const hasDigit = /\d/.test(password)
-      const hasSpecialChar = /[!@#$%^&*()_+{}[\]:;<>,.?~\\-]/.test(password)
-      const length = password.length
-
-      if (
-        hasUppercase &&
-        hasLowercase &&
-        hasDigit &&
-        hasSpecialChar && 
-        length >= 14
-      ) {
-        setPasswordStrength('Fuerte')
-      } else if (
-        hasUppercase &&
-        hasLowercase &&
-        (hasDigit || hasSpecialChar) &&
-        length >= 10 &&
-        length < 14
-      ) {
-        setPasswordStrength('Media')
-      } else {
-        setPasswordStrength('Débil')
-      }
-    }
-
     if (password.length > 0) {
-      checkPasswordStrength()
+      checkPasswordStrength();
     } else {
-      setPasswordStrength('')
+      setPasswordStrength('');
     }
-  }, [password])
+  }, [password, checkPasswordStrength]);
 
   const getBarColor = useCallback(() => {
     switch (passwordStrength) {
       case 'Fuerte':
-        return 'bg-green-600'
+        return 'bg-green-600';
       case 'Media':
-        return 'bg-orange-600'
+        return 'bg-orange-600';
       case 'Débil':
       default:
-        return 'bg-red-600'
+        return 'bg-red-600';
     }
-  }, [passwordStrength])
+  }, [passwordStrength]);
 
   const getBarLength = useCallback(() => {
     switch (passwordStrength) {
       case 'Fuerte':
-        return 'w-[70%]'
+        return 'w-[70%]';
       case 'Media':
-        return 'w-[60%]'
+        return 'w-[60%]';
       case 'Débil':
       default:
-        return 'w-[40%]'
+        return 'w-[40%]';
     }
-  }, [passwordStrength])
+  }, [passwordStrength]);
+
+  const passwordStrengthClasses = classNames(
+    'mt-4 items-center justify-center rounded p-2',
+    getBarColor(),
+    getBarLength()
+  );
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const passwordInputType = showPassword ? 'text' : 'password';
 
   return (
-    <div className=" p-6 font-mono ">
-      {/* <div className="ml-2 rounded bg-gray-300 p-2 text-black hover:bg-gray-400"> */}
-      <h1 className="m-2  text-3xl font-semibold text-slate-400">
+    <div className="css-glow rounded-lg bg-[#2dffc310] p-6 font-mono">
+      <h1 className="m-2 text-3xl font-semibold text-slate-300">
         Validador de seguridad <br /> de contraseñas
       </h1>
       <input
-        type={showPassword ? 'text' : 'password'}
+        type={passwordInputType}
         className="my-3 w-full rounded border border-teal-600 p-3 placeholder-gray-500 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-teal-700"
         placeholder="Introduce tu contraseña"
         value={password}
@@ -82,21 +92,22 @@ export default function PasswordChecker() {
         autoComplete="off"
       />
       <button
-        className="ml-2 mt-3 rounded bg-teal-600  p-2  text-black shadow-lg transition duration-300 ease-in-out hover:scale-x-105 hover:bg-teal-700  active:scale-x-100"
-        onClick={() => setShowPassword(!showPassword)}
+        className="ml-2 mt-3 rounded bg-teal-600 p-2 text-black shadow-lg transition duration-300 ease-in-out hover:scale-x-105 hover:bg-teal-700 active:scale-x-100"
+        onClick={togglePasswordVisibility}
+        title={showPassword ? 'Ocultar contraseña' : 'Mostrar contraseña'}
+        aria-label={showPassword ? 'Ocultar contraseña' : 'Mostrar contraseña'}
       >
         {showPassword ? 'Ocultar' : 'Mostrar'} Contraseña
       </button>
       {passwordStrength && password.length > 0 && (
-        <div className=" flex w-full items-center justify-center ">
-          <div
-            className={`mt-4 items-center justify-center rounded p-2 ${getBarColor()} ${getBarLength()}`}
-          >
-            <p>{passwordStrength}</p>
-          </div>
+        <div
+          className={passwordStrengthClasses}
+          role="status"
+          aria-live="polite"
+        >
+          <p>{passwordStrength}</p>
         </div>
       )}
     </div>
-    // </div>
-  )
+  );
 }
