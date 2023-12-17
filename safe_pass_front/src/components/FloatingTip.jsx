0afcb847f68044c92c43deeb75642f2bdcd16190
Alejandro Martínez Luna <A01276785@tec.mx>
2023-12-17 23:07:47 +0100

    Refactor FloatingButton component to include visitedGuide state and handle navigation

--- safe_pass_front/src/components/FloatingTip.jsx
+++ safe_pass_front/src/components/FloatingTip.jsx
@@ -1,23 +1,36 @@
 // (c) URJC - Safe Pass 2023, rights reserved.
 
+import { useState } from 'react'
 import { FiBookOpen } from 'react-icons/fi'
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 export default function FloatingButton() {
   const navigate = useNavigate()
+  const location = useLocation()
+  const [visitedGuide, setVisitedGuide] = useState(false)
+  const [previousLocation, setPreviousLocation] = useState('')
 
   const handleClick = () => {
-    navigate('/guide')
+    if (!visitedGuide) {
+      setPreviousLocation(location.pathname)
+      navigate('/guide')
+      setVisitedGuide(true)
+    } else {
+      navigate(previousLocation)
+      setVisitedGuide(false)
+    }
   }
 
   return (
-    <button
-      className="fixed right-8 top-20 transform rounded-full bg-blue-500 p-4 text-white shadow-lg transition-transform hover:scale-110 hover:bg-blue-600 focus:outline-none"
-      onClick={handleClick}
-      alt="Tips for secure passwords"
-      title="Tips for secure passwords"
-    >
-      <FiBookOpen size={30} />
-    </button>
+    <div>
+      <button
+        className="fixed right-8 top-20 transform rounded-full bg-blue-500 p-4 text-white shadow-lg transition-transform hover:scale-110 hover:bg-blue-600 focus:outline-none active:scale-100"
+        onClick={handleClick}
+        alt="Tips for secure passwords"
+        title="Tips for secure passwords"
+      >
+        <FiBookOpen size={30} />
+      </button>
+    </div>
   )
 }
