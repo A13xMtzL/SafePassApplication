b934799dde68e0ae379b37c5bc81dac11d888c0b
Alejandro Martínez Luna <A01276785@tec.mx>
2023-12-17 23:07:38 +0100

    Add motion effect to home page images

--- safe_pass_front/src/pages/Home.jsx
+++ safe_pass_front/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 // (c) URJC - Safe Pass 2023, rights reserved.
 
+import { motion } from 'framer-motion'
 import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
@@ -7,7 +8,18 @@ import { Link } from 'react-router-dom'
 export default function Home() {
   const AdditionalImages = () => {
     return (
-      <div className="mt-4 flex justify-center">
+      <motion.div
+        initial={{ x: -250, opacity: 0, scale: 0.5 }}
+        animate={{ x: 0, opacity: 1, scale: 1 }}
+        transition={{
+          // delay: 0.2,
+          stiffness: 60, // Reducir la rigidez para un efecto de rebote más suave
+          duration: 1, // Aumentar la duración para una transición más lenta
+          type: 'spring', // Agregar un efecto de rebote
+          damping: 10, // Controlar la cantidad de rebote
+        }}
+        className="mt-4 flex justify-center"
+      >
         {/* Primera imagen con texto */}
         <Link to="/generator">
           <div className="mx-3 rounded-lg bg-[#cde0e8] pb-7 pt-1 shadow-md transition-transform duration-300 ease-in-out hover:rotate-1 hover:scale-105 dark:bg-teal-800">
@@ -17,7 +29,7 @@ export default function Home() {
                 alt="Generación de contraseña segura"
                 className=" max-w-full"
               />
-              <p className="text-lg ">{t('home-images.generator')}</p>
+              <p className="text-lg">{t('home-images.generator')}</p>
             </div>
           </div>
         </Link>
@@ -30,7 +42,7 @@ export default function Home() {
                 alt="Comprobación de contraseñas"
                 className="max-w-full"
               />
-              <p className="text-lg ">{t('home-images.checker')}</p>
+              <p className="text-lg">{t('home-images.checker')}</p>
             </div>
           </div>
         </Link>
@@ -43,11 +55,11 @@ export default function Home() {
                 alt="Validación de contraseñas"
                 className="max-w-full"
               />
-              <p className="text-lg ">{t('home-images.validator')}</p>
+              <p className="text-lg">{t('home-images.validator')}</p>
             </div>
           </div>
         </Link>
-      </div>
+      </motion.div>
     )
   }
   const [showAdditionalImages, setShowAdditionalImages] = useState(false)
@@ -64,13 +76,18 @@ export default function Home() {
       </h1>
       <div className="max-w-20rem z-10 overflow-hidden rounded-md bg-[#48474d]  shadow-md dark:bg-sky-950">
         <div className="px-4 py-2 ">
-          <h4 className="mb-2 text-lg font-bold text-slate-200">
+          <h4 className="text-lgfont-bold mb-2 text-slate-200">
             {t('home.title')}
           </h4>
           <p className="text-slate-200">{t('home.description')}</p>
         </div>
       </div>
-      <div className="flex items-center justify-center">
+      <motion.div
+        className="flex items-center justify-center"
+        initial="hidden"
+        animate="visible"
+        // variants={buttonVariants}
+      >
         <img
           className="w-40 transform cursor-pointer pt-4 transition-transform duration-300 ease-in-out hover:rotate-12 hover:scale-110"
           src="/Logo.png"
@@ -78,7 +95,7 @@ export default function Home() {
           onClick={toggleAdditionalImages}
         />
         {showAdditionalImages && <AdditionalImages />}
-      </div>
+      </motion.div>
     </div>
   )
 }
